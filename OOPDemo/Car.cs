935a20083bbf859aa0f6958d52bded8d0fcf8699
Chris Cascioli <chriscascioli@gmail.com>
2025-10-03 11:12:52 -0400

    Added OOP Demo

---
+++ OOPDemo/Car.cs
@@ -0,0 +1,51 @@
+﻿// Chris Cascioli
+// 10/3/25
+// Example of a complex data type stored in a class
+
+namespace OOPDemo
+{
+    internal class Car
+    {
+        // The variables of a class are known as "fields"
+        private string make;
+        private string model;
+        private string color;
+        private double mileage;
+
+        /// <summary>
+        /// Creates a car with the specified values
+        /// </summary>
+        /// <param name="make">Car maker</param>
+        /// <param name="model">Specific model</param>
+        /// <param name="color">Paint color</param>
+        public Car(string make, string model, string color)
+        {
+            this.make = make;
+            this.model = model;
+            this.color = color;
+
+            mileage = 0.0;
+        }
+
+        /// <summary>
+        /// Prints car stats
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"{color} {make} {model} has {mileage} miles.");
+        }
+
+        /// <summary>
+        /// Simulates the car driving
+        /// </summary>
+        /// <param name="distance">How far to go</param>
+        public void Drive(double distance)
+        {
+            // Adjust the distance to account for negative values
+            mileage += Math.Abs(distance);
+
+            // Report the results
+            Console.WriteLine($"{model} drives {distance} miles.  Total is {mileage}.");
+        }
+    }
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// Chris Cascioli
+// 10/3/25
+// Example of OOP
+
+namespace OOPDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make a car
+            Car myFirstCar = new Car("VW", "Jetta", "Blue");
+            myFirstCar.Drive(3);
+            myFirstCar.Drive(7);
+            myFirstCar.Drive(400);
+            myFirstCar.Print();
+
+            Car secondCar = new Car("Honda", "Fit", "Black");
+            secondCar.Drive(-7);
+            secondCar.Print();
+        }
+    }
+
+}
